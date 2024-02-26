4d5418d34b9725165780373d73d825fb80e7eca3
Ступеньков Валерий <Valery.Stupenkov@fors.ru>
2024-02-26 20:47:02 +0300

    Add argument -e

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,11 +3,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("-e", type=str)
 parser.add_argument("message", type=str)
 
 args = parser.parse_args()
 
 dict_args = {}
+
+if args.e:
+    dict_args["eyes"] = args.e[:2]
 
 dict_args["message"] = args.message
 print(cowsay(**dict_args))
